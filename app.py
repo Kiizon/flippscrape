982d45690c1a8fdbc73a6950ea8b3d550b16b440
kish dizon <kishddizon@gmail.com>
2025-05-09 20:17:13 -0400

    extract flyer id's of targeted stores

--- app.py
+++ app.py
@@ -23,3 +23,30 @@ def get_flyers_by_postal_code():
     response = requests.get(url)
     response.raise_for_status() 
     return response.json()
+
+def get_grocery_flyer_id():
+    """ Return flyer id's for grocery stores that are labeled as "Groceries" to filter out non-grocery flyers"""
+    response_data = get_flyers_by_postal_code()
+    
+    if "flyers" not in response_data:
+        return None
+        
+    grocery_flyer_ids = []
+    
+    for flyer in response_data['flyers']:
+        merchant = flyer.get('merchant')
+        categories = flyer.get('categories', [])
+        
+        # Convert categories to list if it's a string
+        if isinstance(categories, str):
+            categories = [cat.strip() for cat in categories.split(',')]
+        
+        if merchant in GROCERY_STORES:
+            if "Groceries" in categories:
+                grocery_flyer_ids.append(flyer['id'])
+    
+    return grocery_flyer_ids if grocery_flyer_ids else None
+
+
+flyer_ids = get_grocery_flyer_id()
+print(flyer_ids)
